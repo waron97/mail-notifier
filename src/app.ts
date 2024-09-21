c2523b221a9c087d8ab65043e91d9346c3a7f994
waron97 <winkler.aron5gmail.com>
2024-09-21 09:28:02 +0200

    remove migration block

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,7 @@
 import bodyParser from 'body-parser'
 import express from 'express'
 
-import { KeysCollection } from '@api/keys/model'
 import { PORT } from '@constants'
-import { getClient, getUserInfo } from '@services/google'
 import { scheduleJobs } from '@services/jobs'
 import { startTelegramLoop } from '@services/telegram/loop'
 
@@ -12,19 +10,6 @@ import pages from './pages'
 
 startTelegramLoop()
 scheduleJobs()
-
-for await (const key of KeysCollection.find({})) {
-    const client = await getClient(key.oauth)
-    const userInfo = await getUserInfo(client)
-    await KeysCollection.updateOne(
-        { _id: key._id },
-        {
-            $set: {
-                userInfo,
-            },
-        },
-    )
-}
 
 const app = express()
 app.use(express.static('public'))
